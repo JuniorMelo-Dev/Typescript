87ecdc4a02e86a0029db92edc2c7350c4693668a
JuniorMelo-Dev <juniormelo.dev@gmail.com>
2023-05-20 20:26:06 -0300

    Criando Interface básica a aplicando valores

---
+++ Interfaces/type.ts
@@ -0,0 +1,13 @@
+// Criando Interface e aplicando em uma variável
+
+interface User {
+    name: string;
+    age: 33;
+    active?: boolean;
+}
+
+let user: User = {
+    name: "Junior",
+    age: 33,
+    active: true
+}
